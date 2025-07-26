9f50980f691b792c16a285f652584e33a189dc8f
Rafiadnan0666 <fn234561@gmail.com>
2025-07-26 20:17:18 +0700

    Add file reading and summarization features with improved user prompts

--- main.py
+++ main.py
@@ -29,35 +29,35 @@ def speak(text):
 
 def main():
     """Main function to run the assistant."""
-    speak("Hello! I'm your assistant. How can I help you?")
+    speak("Hello! I'm your assistant. How can I help you today?")
 
     while True:
         command = listen()
 
         if command:
             if "create file" in command:
-                speak("What should be the name of the file?")
+                speak("Sure, what would you like to name the file?")
                 file_name = listen()
                 if file_name:
                     response = actions.create_file(file_name)
                     speak(response)
 
             elif "delete file" in command:
-                speak("What file do you want to delete?")
+                speak("No problem, which file should I delete?")
                 file_name = listen()
                 if file_name:
                     response = actions.delete_file(file_name)
                     speak(response)
 
             elif "create folder" in command:
-                speak("What should be the name of the folder?")
+                speak("Okay, what should I name the new folder?")
                 folder_name = listen()
                 if folder_name:
                     response = actions.create_folder(folder_name)
                     speak(response)
 
             elif "delete folder" in command:
-                speak("What folder do you want to delete?")
+                speak("Alright, which folder do you want to remove?")
                 folder_name = listen()
                 if folder_name:
                     response = actions.delete_folder(folder_name)
@@ -68,12 +68,29 @@ def main():
                 response = actions.open_application(app_name)
                 speak(response)
 
+            elif "read file" in command:
+                speak("Which file would you like me to read?")
+                file_name = listen()
+                if file_name:
+                    content = actions.read_file_content(file_name)
+                    speak("I've read the file. Would you like me to read it out loud or summarize it?")
+                    choice = listen()
+                    if choice and "read" in choice:
+                        speak("Here is the content of the file:")
+                        print(content)
+                        speak(content)
+                    elif choice and "summarize" in choice:
+                        summary = actions.summarize_text(content)
+                        speak("Here is a summary of the file:")
+                        speak(summary)
+                        print(summary)
+
             elif "exit" in command or "quit" in command:
-                speak("Goodbye!")
+                speak("Goodbye! Have a great day!")
                 break
 
             else:
-                speak("Sorry, I don't understand that command.")
+                speak("I'm not sure how to help with that. Can you try another command?")
 
 if __name__ == "__main__":
     main()
